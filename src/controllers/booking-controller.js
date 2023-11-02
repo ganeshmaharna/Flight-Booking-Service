77ffc1fd164952a1beffaf58277838fdd9b4c039
Ganesh Maharna <ganeshmaharna@gmail.com>
2023-11-02 19:41:12 +0530

    Idempotency key logic done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,45 +1,54 @@
-const { StatusCodes } = require('http-status-codes');
-const { BookingService } = require('../services');
-const { SuccessResponse, ErrorResponse } = require('../utils/common');
+const { StatusCodes } = require("http-status-codes");
+const { BookingService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+const inMemDb = {};
 
 async function createBooking(req, res) {
-    try {
-        console.log("body",req.body);
-        const response = await BookingService.createBooking({
-            flightId: req.body.flightId,
-            userId: req.body.userId,
-            noOfSeats: req.body.noOfSeats
-        });
-        SuccessResponse.data = response;
-        return res
-                .status(StatusCodes.OK)
-                .json(SuccessResponse);
-    } catch(error) {
-        console.log("controller catching")
-        ErrorResponse.error = error;
-        return res
-                .status(StatusCodes.INTERNAL_SERVER_ERROR)
-                .json(ErrorResponse);
-    }
+  try {
+    console.log("body", req.body);
+    const response = await BookingService.createBooking({
+      flightId: req.body.flightId,
+      userId: req.body.userId,
+      noOfSeats: req.body.noOfSeats,
+    });
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    console.log("controller catching");
+    ErrorResponse.error = error;
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+  }
 }
 
 async function makePayment(req, res) {
-    try {
-      const response = await BookingService.makePayment({
-        totalCost: req.body.totalCost,
-        userId: req.body.userId,
-        bookingId: req.body.bookingId,
-      });
-      SuccessResponse.data = response;
-      return res.status(StatusCodes.OK).json(SuccessResponse);
-    } catch (error) {
-      ErrorResponse.error = error;
-      console.log("THis is error",error);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+  try {
+    const idempotencyKey = req.headers["x-idempotency-key"];
+    if (!idempotencyKey) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "idempotency key is missing" });
     }
+    if(inMemDb[idempotencyKey]) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({message: 'Cannot retry on a successful payment'});
+    } 
+    const response = await BookingService.makePayment({
+      totalCost: req.body.totalCost,
+      userId: req.body.userId,
+      bookingId: req.body.bookingId,
+    });
+    inMemDb[idempotencyKey] = idempotencyKey;
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    console.log("THis is error", error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
   }
-
-module.exports={
- createBooking,
- makePayment
 }
+
+module.exports = {
+  createBooking,
+  makePayment,
+};
